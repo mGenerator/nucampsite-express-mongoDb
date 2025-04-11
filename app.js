7c5df143366b942236451f804ba6b9f1f0e3b3a9
Calherbe Valcin <mgenerator01@hotmail.com>
2025-04-11 14:31:00 -0400

    Cookies

--- app.js
+++ app.js
@@ -1,83 +1,95 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const campsiteRouter = require('./routes/campsiteRouter');
-const partnerRouter = require('./routes/partnerRouter');
-const promotionRouter = require('./routes/promotionRouter');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const campsiteRouter = require("./routes/campsiteRouter");
+const partnerRouter = require("./routes/partnerRouter");
+const promotionRouter = require("./routes/promotionRouter");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const url = 'mongodb://127.0.0.1:27017';
+const url = "mongodb://127.0.0.1:27017";
 
 const connect = mongoose.connect(url, {});
 
-connect.then(()=> console.log('Connected correctly to the server.'),
-err => console.log(err)
+connect.then(
+  () => console.log("Connected correctly to the server."),
+  (err) => console.log(err)
 );
-
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser("12345-67890-09876-54321"));
 
-function auth(req, res, next){
-  console.log(req.headers);
-  const authHeader = req.headers.authorization;
-  if(!authHeader){
-    const err = new Error('You are not authenticated');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
-  }
-  //important, research for later challenge question
-  const auth = Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
-  const user = auth[0];
-  const pass = auth[1];
-  if(user === 'admin' && pass === 'password'){
-    return next(); //authorized
+function auth(req, res, next) {
+  if (!req.signedCookies.user) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      const err = new Error("You are not authenticated");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
+    //important, research for later challenge question
+    const auth = Buffer.from(authHeader.split(" ")[1], "base64")
+      .toString()
+      .split(":");
+    const user = auth[0];
+    const pass = auth[1];
+    if (user === "admin" && pass === "password") {
+      res.cookie('user','admin',{signed: true});
+      return next(); //authorized
+    } else {
+      const err = new Error("You are not authorized.");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
   }else{
-    const err = new Error('You are not authorized');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
+    if(req.signedCookies.user === 'admin'){
+      return next();
+    }else{
+      const err = new Error('You are not authenticated!');
+      err.status = 401;
+      return next(err);
+    }
   }
 }
 app.use(auth);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //set up endpoints with routers
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/campsites', campsiteRouter);
-app.use('/partners', partnerRouter);
-app.use('/promotions',promotionRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/campsites", campsiteRouter);
+app.use("/partners", partnerRouter);
+app.use("/promotions", promotionRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
